6ce26b89c44f5d1505e9d7fcf7c09865cc09c8bd
pziecina <pziecin@gmail.com>
2021-08-03 17:15:14 +0200

    Test class

---
+++ Test/com.pziecin/PredictorTest.java
@@ -0,0 +1,13 @@
+package com.pziecin;
+
+import com.pziecin.Mechanic.Predictor;
+import com.pziecin.Mechanic.State;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PredictorTest {
+
+    @Test
+    void solveStatesFor2Players() {
+    }
+}
